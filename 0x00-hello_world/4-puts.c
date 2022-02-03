9d6ba64148582d78e0f897e54a348707ead77be8
ChrissLind <chrislindino@gmail.com>
2022-02-03 18:38:42 +0000

    4 Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - function that prints the correct output
- * Write a C program that prints exactly \"Programming is like 
+ * Write a C program that prints exactly \"Programming is like
  * building a multilingual puzzle, followed by a new line.
  *
  * Return: 0
