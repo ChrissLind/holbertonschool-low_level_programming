eadd5341890f317ecc84cbc97b62125f98048f5e
ChrissLind <chrislindino@gmail.com>
2022-02-05 02:34:02 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
