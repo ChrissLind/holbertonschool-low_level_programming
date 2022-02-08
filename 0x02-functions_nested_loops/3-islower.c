1183c6674bba9822159cdbf3bf6c19569d6446f2
ChrissLind <chrislindino@gmail.com>
2022-02-08 23:53:30 +0000

    changed task 3 and 7

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - function that checks for lowercase character
  *
- * @c: Return 1 if c is lowercase
+ * @c: The letter being checked
  *
  * Return: 1 if c is lowercase, 0 if not.
  */
