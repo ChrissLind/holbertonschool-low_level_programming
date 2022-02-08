24065f56fc2c56a32a73475846e59f1afe53026a
ChrissLind <chrislindino@gmail.com>
2022-02-08 17:38:34 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - function that prints the aplhabet
+ *
+ */
+
+void print_alphabet(void)
+{
+	char al;
+
+	for (al = 'a' ; al <= 'z' ; al++)
+
+		_putchar(al);
+
+	_putchar('\n');
+
+}
