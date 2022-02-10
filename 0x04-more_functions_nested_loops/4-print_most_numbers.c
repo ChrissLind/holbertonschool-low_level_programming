b5ed2b989fa9d39bdd1363067dc0e669b2b13fb0
ChrissLind <chrislindino@gmail.com>
2022-02-10 14:40:30 -0400

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,15 @@
 void print_most_numbers(void)
 {
 	int n;
+
 	n = 0;
+
 	{
 		for (n = n; n <= 9; n++)
 			if ((n != 2) && (n != 4))
-		{
-			_putchar(n + '0');
-		}
+			{
+				_putchar(n + '0');
+			}
 	}
 	_putchar('\n');
 }
