f1fa8f14366de3761aae2625a07c4f3269114930
ChrissLind <chrislindino@gmail.com>
2022-02-14 10:27:40 -0400

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Function that swaps the value of two integers.
+ * 
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
