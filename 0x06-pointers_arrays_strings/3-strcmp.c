ccff2abcdb7d0a451d4e3447491aff4011899220
ChrissLind <chrislindino@gmail.com>
2022-02-17 13:28:22 -0400

    task 3 betty fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- *
- *
- *
- * @
+ * _strcmp - Function that compares 2 strings.
+ * @s1: String 1.
+ * @s2: String 2.
  *
  * Return: 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 != '\0' && *s2 !='\0') && *s1 == *s2)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
