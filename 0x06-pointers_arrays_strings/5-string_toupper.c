3699aa1bc803cc21450cf32ea41b3cbfdabd1336
ChrissLind <chrislindino@gmail.com>
2022-02-17 15:20:06 -0400

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ * @
+ *
+ * Return: 0
+ */
+
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] = a[i] - 32;
+	}
+	return (a);
+}
