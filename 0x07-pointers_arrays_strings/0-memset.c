b0abd01ccb17bc3d08c469a25a84175eb5c1a0fb
ChrissLind <chrislindino@gmail.com>
2022-02-22 13:54:11 -0400

    task 0 fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,6 +3,9 @@
 /**
  * *_memset - Function that fills memory with a consistant byte.
  *
+ * @s: pointer
+ * @b: character
+ * @n: unsigned int
  * Return: 0
  */
 
@@ -12,5 +15,5 @@ char *_memset(char *s, char b, unsigned int n)
 
 	for (count = 0; count < n; count++)
 		s[count] = b;
-	return s;
+	return (s);
 }
