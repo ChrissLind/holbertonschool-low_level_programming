6050f5d1a368e6731d2ceb447d7b880799fc462c
ChrissLind <chrislindino@gmail.com>
2022-02-25 14:33:17 -0400

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Program that prints its name.
+ *
+ *
+ */
+
+int main(int __attribute__((unused))argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+		return (0);
+}
