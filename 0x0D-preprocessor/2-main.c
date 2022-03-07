0070f5429ec462cfc3d5e38fd3ac72984e90dc6b
ChrissLind <chrislindino@gmail.com>
2022-03-07 14:19:23 -0400

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *
- * main - Function that prints the name of a file
+ * main - Function that prints the name of a file.
  *
  * Return: 0
  */
 
-int main()
+int main(void)
 {
 	printf("%s" __FILE__);
 
