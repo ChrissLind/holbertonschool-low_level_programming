b85438a6a0f10bf66fb503785ac071065f7c8924
ChrissLind <chrislindino@gmail.com>
2022-03-08 11:26:04 -0400

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - Function that free's dogs.
+* @d: Dog object
+* Return: 0
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d = NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
+
